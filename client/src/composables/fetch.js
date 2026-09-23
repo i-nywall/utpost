7a7cd9d7e14f3ecce278214d0d93565b8be0dc39
Isak Nywall <nywall@pm.me>
2026-09-23 18:37:58 +0200

    client: port first view

---
+++ client/src/composables/fetch.js
@@ -0,0 +1,26 @@
+import { ref, watchEffect, toValue } from 'vue'
+
+export function useFetch(url) {
+  const data = ref(null)
+  const error = ref(null)
+
+  const fetchData = () => {
+    // reset state before fetching..
+    data.value = null
+    error.value = null
+
+    fetch(toValue(url))
+      .then((res) => {
+        if (!res.ok) throw new Error(`Network response was not ok ${res.status}`)
+        return res.json()
+      })
+      .then((json) => (data.value = json))
+      .catch((err) => (error.value = err))
+  }
+
+  watchEffect(() => {
+    fetchData()
+  })
+
+  return { data, error }
+}
